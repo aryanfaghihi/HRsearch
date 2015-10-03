bd51ec8ea6563f44230a25842ba91ab4f284746e
Aryan <aryanfaghihi@gmail.com>
2015-10-04 00:10:02 +1000

    Made a few changes to the app engine. The Google search and LinkedIn scraper are now connected.

--- app.js
+++ app.js
@@ -66,29 +66,67 @@ var app = express();
 
 ///////////////////////////////////////////////////
 // Google Results Scraper
-/*
+
 var googleScraper = require('google-search-scraper');
 
 var options = {
-    query: 'Aryan Faghihi site:linkedin.com',
+    query: 'janith randeniya site:linkedin.com',
     limit: '10'
 };
 var googleResults = [];
+var callBacks = 0;
 googleScraper.search(options, function (err, url) {
     if (err) throw err;
+    callBacks++;
     googleResults.push(url);
-    console.log(googleResults);
+
+    if (callBacks === 10) {
+        console.log(googleResults);
+        callBacks = 0;
+
+        var searchUrl = googleResults[0];
+        var scrape = require('scrape-url');
+        var keywords = ['.full-name', '.title', '.locality',
+            '.education header h4', '.education header h4 a',
+            '.background-experience div div header h4 a'];
+
+        scrape(searchUrl, keywords,
+            function (error, name, title, locality, educationWithoutLink, educationWithLinks,
+                      experienceWithLink) {
+
+                console.log('error: ' + error);
+                console.log(name[0].html());
+                console.log(title[0].html());
+                console.log(locality[0].html());
+                // Getting Education data
+                for (var i = 0; i < educationWithoutLink.length; i++) {
+                    if (educationWithoutLink[i].html().indexOf('<a href') == undefined) {
+                        console.log(educationWithoutLink[i].html())
+                    }
+                }
+                for (var i = 0; i < educationWithLinks.length; i++) {
+                    console.log(educationWithLinks[i].html())
+                }
+                // Getting experience data
+                for (var i = 0; i < experienceWithLink.length; i++) {
+                    console.log(experienceWithLink[i].html());
+                }
+
+
+            });
+    }
 });
-*/
+
 
 // LinkedIn API Scraper /////////////////////////////////////////////
 /*
+ var searchUrl = googleResults[0];
  scrape = require('scrape-url');
  var keywords = ['.full-name', '.title', '.locality',
  '.education header h4', '.education header h4 a',
  '.background-experience div div header h4 a'];
 
- scrape('https://au.linkedin.com/in/rayemagp', keywords,
+ scrape(searchUrl, keywords,
  function (error, name, title, locality, educationWithoutLink, educationWithLinks,
  experienceWithLink) {
 
@@ -107,14 +145,12 @@ googleScraper.search(options, function (err, url) {
  }
  // Getting experience data
  for (var i = 0; i < experienceWithLink.length; i++) {
- console.log(experienceWithLink[i].html())
+ console.log(experienceWithLink[i].html());
  }
 
 
  });
  */
-
-
 
 
 // view engine setup
